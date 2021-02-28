08f082d262453980a089c37524414c5a8176e3d4
Moktahid Al faisal <moktahid.al-faisal@stud.uni-bamberg.de>
2021-02-28 19:20:19 +0100

    second commit

--- scripts.js
+++ scripts.js
@@ -9,6 +9,6 @@ var links = ['https://www.youtube.com/embed/dOgBUAn6x9U',
     'https://www.youtube.com/embed/dOgBUAn6x9U',
     'https://www.youtube.com/embed/dOgBUAn6x9U']
 for (let i = 0; i <links.length ; i++) {
-    document.getElementById('iframe'+i).src = sites[i];
+    document.getElementById('iframe'+i).src = links[i];
 }
 
