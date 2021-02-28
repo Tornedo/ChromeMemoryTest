3dd88700bdbd64879516ba17b089132564a554f3
Moktahid Al faisal <moktahid.al-faisal@stud.uni-bamberg.de>
2021-02-28 19:09:46 +0100

    first commit

---
+++ scripts.js
@@ -0,0 +1,14 @@
+
+var links = ['https://www.youtube.com/embed/dOgBUAn6x9U',
+    'https://www.youtube.com/embed/dOgBUAn6x9U',
+    'https://www.youtube.com/embed/dOgBUAn6x9U',
+    'https://www.youtube.com/embed/dOgBUAn6x9U',
+    'https://www.youtube.com/embed/dOgBUAn6x9U',
+    'https://www.youtube.com/embed/dOgBUAn6x9U',
+    'https://www.youtube.com/embed/dOgBUAn6x9U',
+    'https://www.youtube.com/embed/dOgBUAn6x9U',
+    'https://www.youtube.com/embed/dOgBUAn6x9U']
+for (let i = 0; i <links.length ; i++) {
+    document.getElementById('iframe'+i).src = sites[i];
+}
+
